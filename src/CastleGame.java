d76db1f6f16035fea868e06449dc5c78939ca78a
BT_1E10_33 <sample@bths.edu>
2022-05-13 12:25:52 -0400

    idk what to say tbh

--- src/CastleGame.java
+++ src/CastleGame.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class CastleGame {
     private String[][] castle;
+    private Archetype player;
 
     public CastleGame(String difficultyMode)
     {
@@ -21,9 +22,53 @@ public class CastleGame {
     public void play()
     {
         Scanner asker = new Scanner(System.in);
-        System.out.println("WELCOME TO CASTLE HASSLE!\nPlease choose your difficulty before proceeding forward! (type \"easy\" for easy mode, \"normal\" for normal mode, and \"hard\" for hard mode)");
-        String input = asker.nextLine();
-        CastleGame game = new CastleGame(input);
-        System.out.println("Please select your class!");
+        System.out.println("What is your name?");
+        String myNameIs = asker.nextLine(); //chicka chicka slim shady
+        System.out.println("Hello, " + myNameIs + ", what is your class? (Pirate, Knight, Ninja)");
+        String chosenClass = asker.nextLine();
+        if(chosenClass.equals("Pirate"))
+        {
+            String archo = "";
+            while(!(archo.equals("Drunkard") || archo.equals("Plunderer") || archo.equals("Cannoneer")))
+            {
+                System.out.println("Choose an archetype!(Drunkard, Plunderer, Cannoneer)");
+                archo = asker.nextLine();
+            }
+            if(archo.equals("Drunkard"))
+            {
+                player = new Drunkard(myNameIs);
+            }
+            if(archo.equals("Cannoneer"))
+            {
+                player = new Cannoneer(myNameIs);
+            }
+            if(archo.equals("Plunderer"))
+            {
+                player = new Plunderer(myNameIs);
+            }
+        }
+        if(chosenClass.equals("Knight"))
+        {
+            player = new Knight(myNameIs);
+        }
+        if(chosenClass.equals("Ninja"))
+        {
+            //player = new Ninja(myNameIs);
+        }
+        while(player.getHP() > 0)
+        {
+            for(int r = castle.length-1; r >= 0; r--)
+            {
+                for(int c = 0; c < castle[r].length; c++)
+                {
+                    castle[r][c] = "0\n/|\\\n/\\";
+                    System.out.println(castle);
+                }
+            }
+        }
+        if(player.getHP() == 0)
+        {
+            System.out.println("DECEASED!");
+        }
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class Runner {
     public static void main(String[] args) {
+        Scanner asker = new Scanner(System.in);
+        String input = "";
+        while(!(input.equals("easy") || input.equals("normal") || input.equals("hard"))) {
+            System.out.println("WELCOME TO CASTLE HASSLE!\nPlease choose your difficulty before proceeding forward! (type \"easy\" for easy mode, \"normal\" for normal mode, and \"hard\" for hard mode)");
+            input = asker.nextLine();
+        }
+        CastleGame game = new CastleGame(input);
+        game.play();
+
 
     }
 }
